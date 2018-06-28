d11f774ac10ffb352a097111bd6d763dc3083067
NIKMC <nikmc10@mail.ru>
2018-06-28 23:47:22 +0300

    use data binding

---
+++ MVVM-2/Box.swift
@@ -0,0 +1,29 @@
+//
+//  Box.swift
+//  MVVM-2
+//
+//  Created by Ivan Nikitin on 28/06/2018.
+//  Copyright © 2018 Иван Никитин. All rights reserved.
+//
+
+import Foundation
+
+class Box<T> {
+    
+    typealias Listener = (T) -> ()
+    var listener: Listener?
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    func bind(listener: @escaping Listener) {
+        self.listener = listener
+        listener(value)
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+}
--- MVVM-2/View/DetailViewController.swift
+++ MVVM-2/View/DetailViewController.swift
@@ -21,5 +21,21 @@ class DetailViewController: UIViewController {
         
         self.textLabel.text = viewModel.description
     }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel?.age.bind{ [unowned self] in
+            guard let string = $0 else { return }
+            self.textLabel.text = string
+        }
+        delay(delay: 5) { [unowned self] in
+            self.viewModel?.age.value = "Some new value"
+        }
+        
+    }
     
+    private func delay(delay: Double, closure: @escaping ()->()) {
+        DispatchQueue.main.asyncAfter(wallDeadline: .now() + delay) {
+            closure()
+        }
+    }
 }
